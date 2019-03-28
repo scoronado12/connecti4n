c9a90f3c0a5861257372496469bd5bf098dc8011
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-03-28 13:04:48 -0400

    sending/recv messages as client

--- connecti4n_client.py
+++ connecti4n_client.py
@@ -8,12 +8,53 @@
 
 import socket
 
+VERSION = '1.0'
+
+# Send to server
+def c4n_send_msg(codigo, contenido):
+    #make sure there is a valid code
+    if (codigo not in  ['ERROR', 'STOP', 'START', 'MOVE', 'BOARD', 'RESULT']):
+        return None
+    
+    # construct basic message
+    
+    out = 'C4N ' + VERSION + ' ' + code
+    
+    # append content if there is anything
+    
+    if (content != None):
+        out += ' ' + str(content)
+    
+    #return generated headers
+    
+    return out.encode()
+    
+def c4n_get_msg(sock):
+    #TODO pass back a string containing the value of out from server
+    
+    
+    msg = sock.recv(1024)
+    
+    return msg.decode()
+    
+
 
 def main():
     HOST = 'localhost'
-    PORT = '4414'
+    PORT = 4414
 
-    pass
-
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    sock.connect((HOST, PORT))
+    
+    message_from_server = c4n_get_msg(sock)
+    
+    print(message_from_server)
+        
+        
+    
+    sock.close()
+    
+    
 
 main()
