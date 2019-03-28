15b60485695d8ae22cb721b88062039b856e5e43
Ethan Martin <ethan@ethanmartin.net>
2019-03-28 11:56:21 -0400

    add server skeleton

---
+++ connecti4n_server.py
@@ -0,0 +1,19 @@
+# File:   connecti4n_server.py
+# Author: Ethan Martin & Stefano Coronado
+# Date:   28 March 2019
+# Brief:  Server for a game of Connect I4n
+#
+# We hereby declare on our word of honor that we have neither given nor
+# received any unauthorized help on this work.
+
+import socket
+
+
+def main():
+    HOST = 'localhost'
+    PORT = '4414'
+
+    pass
+
+
+main()
