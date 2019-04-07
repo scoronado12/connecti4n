2c0fa5098703c457081e015bd39a424c1c1e4abe
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-04-06 20:48:21 -0400

    added some verification to start a game

--- connecti4n_client.py
+++ connecti4n_client.py
@@ -12,14 +12,10 @@ VERSION = '1.0'
 
 MSG_CODES = ['ERROR', 'STOP', 'START', 'MOVE', 'BOARD', 'RESULT']
 
-#GAME = [['x','x','x','x','x','x','x'],
-        #['x','x','x','x','x','x','x'],
-        #['x','x','x','x','x','x','x'],
-        #['x','x','x','x','x','x','x'],
-        #['x','x','x','x','x','x','x'],
-        #['x','x','x','x','x','x','x']]
-        
-#print(GAME)
+# Send start command to server
+def c4n_send_start(sock):
+    out = 'C4N ' + VERSION +' ' + MSG_CODES[2]
+    sock.sendall(out.encode())
 
 
 # Send to server
@@ -33,46 +29,40 @@ def c4n_send_msg(codigo, contenido, sock):
         # append content if there is anything on the 
         if (contenido != None):
             out += '\n' + str(contenido)
-            
         print(out)
         #send all generated headers
         sock.sendall(out.encode())
-    
+
 def c4n_get_msg(sock):
     #TODO pass back a string containing the value of out from server
-    
-    msg = sock.recv(1024)
-    
-    return msg.decode()
-    
 
+    msg = sock.recv(1024)
+
+    return msg.decode()
 
 def main():
-    HOST = 'localhost'
+    HOST = input("Welcome to Connecti4n!\nWhat is the server IP address? ")
     PORT = 4414
+
+
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
         sock.connect((HOST, PORT))
-        while True:
-            message_to_server = input("Test a message ")
-            #as is it will pass in the start command
-            c4n_send_msg(MSG_CODES[2], message_to_server, sock)
-    
-    
-            message_from_server = c4n_get_msg(sock)
-    
-    
-    
-            print(message_from_server)
+        start = input("Would you like to start the game?")
+        if (start == 'y'):
+            while True:
+                message_to_server = input("Test a message ")
+                #as is it will pass in the start command
+                #c4n_send_msg(MSG_CODES[2], message_to_server, sock)
+                message_from_server = c4n_get_msg(sock)
+                print(message_from_server)
+        else:
+            exit(0)
     except (KeyboardInterrupt):
         sock.close()
     except ConnectionRefusedError:
-        print("The server you are trying to connect to is not found")
-        exit(1)
-    
+         print("The server you are trying to connect to is not found")
+         exit(1)
     sock.close()
-    
-    
 
 main()
