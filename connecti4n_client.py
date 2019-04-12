c73ec66f47cfb919ca635ef3ff64ba448cf74dca
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-04-12 13:11:39 -0400

    fixed issues with board flatten, tweaked client logic

--- connecti4n_client.py
+++ connecti4n_client.py
@@ -12,20 +12,56 @@ VERSION = '1.0'
 
 MSG_CODES = ['ERROR', 'STOP', 'START', 'MOVE', 'BOARD', 'RESULT']
 
+
+def main():
+    HOST = 'localhost'#input("Welcome to Connecti4n!\nWhat is the server IP address? ")
+    PORT = 4414
+
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        start = input("Would you like to start the game? ")
+        if (start == 'y'):
+            # send start command to server
+            c4n_send_start(sock)
+            #winner = False #TODO maybe replace this with a detect winner packet from server
+
+            while True:
+                move_magic(sock)
+
+
+                #winner = True #break loop while debugging
+
+            # Game exits when exiting from loop
+            sock.close()
+            exit(0)
+        else:
+            sock.close()
+            exit(0)
+    except KeyboardInterrupt:
+        sock.close()
+    except ConnectionRefusedError:
+         print("The server you are trying to connect to is not found")
+         exit(1)
+    sock.close()
+
+
+
 def c4n_validate(data):
     # Decode the data
     lines = data.decode().split('\n')
 
     # Extract the header
     header = lines[0].split()
+    
     # Validate length
     if (len(header) != 3 or
         header[0] != 'C4N' or
         header[1] != VERSION or
         header[2] not in MSG_CODES):
-        # TODO fail
         print('Bad message')
-        return None
+        raise Exception
 
     # Read the content if present
     if len(lines) > 1:
@@ -63,18 +99,19 @@ def c4n_message(code, content):
 
 # Board Data
 def board_unflatten(board_data):
-    if board_data[0] != 'BOARD':
-        print("Not a BOARD packet")
-        return None
-    else:
-        board = board_data[1][4:]
-
-        n = 14
-        board = [board[i:i+n] for i in range(0, len(board), n)]
-        print("A B C D E F G")
-        for list in board:
-            print(list)
-        return board
+    
+    #if board_data[0] != 'BOARD':
+        #print("Not a BOARD packet")
+        #return None
+    #else:
+    print("bd",board_data)
+    board = board_data [4:]
+    print("b", board)
+    board = [board[i:i+14] for i in range(0, len(board), 14)]
+    print("A B C D E F G")
+    for list in board:
+        print(list)
+    return board
 
 
 ## Turns the user's selection into a proper char that the server can read
@@ -89,59 +126,36 @@ def letterInNumOut(moveLetter):
     return None
 
 def move_magic(sock):
+    print("AI Turn")
+    recieve_packet = sock.recv(1024) #get board/error/result from server
+    whatIs_recieve = c4n_validate(recieve_packet)
+    if (whatIs_recieve[0] == 'BOARD'):
+        
+        current_board = board_unflatten(whatIs_recieve[1]) #This is a list
+        move = input("Which is your move? ") #take the move
 
-    board_data = c4n_validate(sock.recv(1024)) #get board from server
-    current_board = board_unflatten(board_data) #This is a list
-    move = input("Which is your move? ") #take the move
-    move = letterInNumOut(move)
+        move = letterInNumOut(move)
+        #send Move Packet back
 
-
-    #send Move Packet back
-
-    sock.sendall(c4n_message('MOVE', move))
-    # recieve is valid packet
-    validity = c4n_validate(sock.recv(1024))
-
-    if validity != ('ERROR' ,'2'):
-        pass
-    else:
-        # validity recieves an error packet
-        print("Try again!")
-        move_magic(sock)
+        sock.sendall(c4n_message('MOVE', move))
+        # recieve is valid packet
+        
 
 
-
-def main():
-    HOST = 'localhost'#input("Welcome to Connecti4n!\nWhat is the server IP address? ")
-    PORT = 4414
-
-
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((HOST, PORT))
-        start = input("Would you like to start the game? ")
-        if (start == 'y'):
-            # send start command to server
-            c4n_send_start(sock)
-            winner = False #TODO maybe replace this with a detect winner packet from server
-
-            while winner != True:
-                move_magic(sock)
+    elif (whatIs_recieve[0] == 'ERROR'):
+        print("An Error came in")
+        raise KeyboardInterrupt
+    
+    elif (whatIs_recieve[0] == 'RESULT'):
+        
+        if whatIs_recieve[1] == '1':
+            print("You Win!\nConnect I4n!")
+        elif whatIs_recieve[1] == '2':
+            print("You Lose!\nConnect I4n!")
 
 
-                winner = True #break loop while debugging
 
-            # Game exits when exiting from loop
-            sock.close()
-            exit(0)
-        else:
-            sock.close()
-            exit(0)
-    except KeyboardInterrupt:
-        sock.close()
-    except ConnectionRefusedError:
-         print("The server you are trying to connect to is not found")
-         exit(1)
-    sock.close()
+
+
 
 main()
